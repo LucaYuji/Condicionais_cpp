cde4580e24a74ac670412f8626ce45213e31f966
Lucas Yuji <160159812+LucaYuji@users.noreply.github.com>
2024-07-03 09:54:56 -0300

    Add files via upload

---
+++ if_else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int num = 10;
+    int num2 = 50;
+    char opc = 'a';
+
+    //se(teste lógico)
+    if(num > num2){
+        cout << 'Resultado verdadeiro';
+    }else{
+        cout << 'Resultado falso';
+    }
+    
+    return 0;
+}
